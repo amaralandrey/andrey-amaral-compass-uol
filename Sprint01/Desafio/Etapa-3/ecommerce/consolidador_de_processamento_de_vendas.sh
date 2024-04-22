6352abbadbd3787c19364c51bb4c7da68e8ae28a
Andrey Amaral <118300118+amaralandrey@users.noreply.github.com>
2024-04-22 17:13:47 -0300

    Update consolidador_de_processamento_de_vendas.sh

--- Sprint01/Desafio/Etapa-3/ecommerce/consolidador_de_processamento_de_vendas.sh
+++ Sprint01/Desafio/Etapa-3/ecommerce/consolidador_de_processamento_de_vendas.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Define o nome do arquivo final
 relatorio_final="relatorio_final.txt"
 
 cd /home/andrey/andrey-amaral-compass-uol-1/Sprint01/Desafio/Etapa-3/ecommerce/vendas/backup
 
-# Percorre todos os arquivos de relatório e os une no arquivo final
 for relatorio in *.txt; do
     cat "$relatorio" >> "../$relatorio_final"
     echo "" >> "../$relatorio_final"
 done
-
